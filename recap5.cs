6b080cb744ad49e407f3982e413d580d6dc71692
Ntokozo <ntokntshangase@gmail.com>
2024-11-28 16:56:50 +0200

    Create recap5.cs
    Write some script to produce the following:

---
+++ recap5.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("And the final goal was score by Eric \"The King\" Cantona");
+        }
+    }
+}
