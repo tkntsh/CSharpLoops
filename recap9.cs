e14556e091ab515e3a691a42dc6bd4a8a4f0dafc
Ntokozo <ntokntshangase@gmail.com>
2024-11-28 17:00:26 +0200

    Create recap9.cs
    Create a Console which prints the following script: 
    
    Judge: "Mr. Carp, the prosecution may call the first witness."
    Mr. Carp: "Your honor, the prosection would like to call Rene Descartes."
    Judge: "Bailiff, call Rene Descartes"
    Bailiff: "Rene Descartes, born 1896!"

---
+++ recap9.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Judge:   \"Mr. Carp, the production may call the first witness.\"" +
+                "\nMr Carp: \"Your honor, the prosecution would like to call Rene Descartes.\"" +
+                "\nJudge:   \"Bailiff, call Rene Descartes.\"" +
+                "\nBailiff: \"Rene Descartes, born 1896!\"");
+        }
+    }
+}
