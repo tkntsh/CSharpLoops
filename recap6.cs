bd415637a099d6b7daf2c261936db2d12d3a761f
Ntokozo <ntokntshangase@gmail.com>
2024-11-28 16:57:17 +0200

    Create recap6.cs
    Write some code to find if 44.90 is greater than 14.60 and 189 is less than 100.
    Print “The answer is TRUE” if true.

---
+++ recap6.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            if(44.90>14.60 && 189<100)
+            {
+                Console.WriteLine("This answer is TRUE");
+            }
+            else
+            {
+                Console.WriteLine("This answer is FALSE");
+            }
+        }
+    }
+}
