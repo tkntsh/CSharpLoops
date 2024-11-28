19d35c3b8940880a6e6557f46f5b8b43612f08c3
Ntokozo <ntokntshangase@gmail.com>
2024-11-28 16:40:01 +0200

    Create loops1.cs
    Write a for loop which prints 1-10 in the Console.

---
+++ loops1.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace loops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i = 1; i<11; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
