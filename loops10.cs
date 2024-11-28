bca6c505ade25d9b17d16defefc7ce38eb9febdd
Ntokozo <ntokntshangase@gmail.com>
2024-11-28 16:44:10 +0200

    Create loops10.cs
    Write a while loop which prints the string “HELLO” whilst the value of x is less than 5.
    Initialise x with the value 0.

---
+++ loops10.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace loops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 0;
+
+            while(x<5)
+            {
+                Console.WriteLine("HELLO");
+                x++;
+            }
+        }
+    }
+}
