5581b0dd56535642d350d993e4113349c1d71ff1
Ntokozo <ntokntshangase@gmail.com>
2024-11-28 16:41:16 +0200

    Create loops4.cs
    Write a for loop which prints only odd numbers between 100-1 in the Console.

---
+++ loops4.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace loops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i = 99; i>0; i-=2)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
