85c07897d21d118d9da0a3a25c7acadd0f0d5456
Ntokozo <ntokntshangase@gmail.com>
2024-11-28 16:43:47 +0200

    Create loops9.cs
    Write a while loop which prints 10-1 in the Console.

---
+++ loops9.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace loops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 10;
+
+            while(i>0)
+            {
+                Console.WriteLine(i);
+                i--;
+            }
+        }
+    }
+}
