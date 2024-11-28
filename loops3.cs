eeb72fa5962e22a8ac687ea6f44e1c1cb6287380
Ntokozo <ntokntshangase@gmail.com>
2024-11-28 16:40:51 +0200

    Create loops3.cs
    Write a for loop which prints only even numbers between 1-100 in the Console.

---
+++ loops3.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace loops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i = 2; i<101; i+=2)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
